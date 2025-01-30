f335c90be9cd4622cf88266b5aa7ff133dbbd795
Shanmukha Pranav <147685740+PranavKuruvella@users.noreply.github.com>
2025-01-30 19:31:02 +0530

    Delete expressError.js

--- expressError.js
+++
@@ -1,8 +0,0 @@
-class expressError extends Error{
-    constructor(status,message){
-        super()
-        this.message = message
-        this.status = status
-    }
-}
-module.exports = expressError
